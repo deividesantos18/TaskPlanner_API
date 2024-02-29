0e652ed6edda7c66400ceb7f3accb79531705056
deivide <deividesantos18>
2024-02-29 06:36:23 -0300

    Create Service

---
+++ src/main/java/com/deividesantos/todosimple/controllers/UserService.java
@@ -0,0 +1,63 @@
+package com.deividesantos.todosimple.controllers;
+
+import com.deividesantos.todosimple.models.User;
+import com.deividesantos.todosimple.repositories.TaskRepository;
+import com.deividesantos.todosimple.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository objUserRepository;
+
+    @Autowired
+    private TaskRepository objTaskRepository;
+
+
+    public User findbyid(Long id){
+        Optional<User> objuseroptional=this.objUserRepository.findById(id);
+        return objuseroptional.orElseThrow(()-> new RuntimeException("Usuario não encontrado! id:"
+        +id+",Tipo"+User.class.getName()
+        ));
+    }
+
+
+    @Transactional
+    public User createUser(User objuser){
+        objuser.setId(null);
+        objuser = this.objUserRepository.save(objuser);
+        this.objTaskRepository.saveAll(objuser.getTasks());
+        return objuser;
+
+    }
+
+
+    @Transactional
+    public User updateuser(User objuser){
+        User newobj= findbyid(objuser.getId());
+        newobj.setPassword(objuser.getPassword());
+        return this.objUserRepository.save(newobj);
+
+
+    }
+
+
+    public void delete(Long id){
+        findbyid(id);
+        try{
+         this.objUserRepository.deleteById(id);
+        }catch (Exception e){
+           throw new RuntimeException("Não é possivel deletar usuario");
+        }
+
+
+    }
+
+
+
+}
