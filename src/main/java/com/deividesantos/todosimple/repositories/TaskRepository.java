3a9147787321facbee81fb875ca9e82ae803fd29
deivide <deividesantos18>
2024-02-28 22:07:18 -0300

    Create User and Task Repository

---
+++ src/main/java/com/deividesantos/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,14 @@
+package com.deividesantos.todosimple.repositories;
+
+import com.deividesantos.todosimple.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task,Long> {
+
+    List<Task> findByUser_id(Long id);
+
+}
---
+++ src/main/java/com/deividesantos/todosimple/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.deividesantos.todosimple.repositories;
+
+import com.deividesantos.todosimple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
