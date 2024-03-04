c6d36970660ecb4064a192b11ae56e828e3cc2a0
deivide <deividesantos18>
2024-03-04 19:35:00 -0300

    Create WebConfig

--- src/main/java/com/deividesantos/todosimple/models/User.java
+++ src/main/java/com/deividesantos/todosimple/models/User.java
@@ -1,5 +1,6 @@
 package com.deividesantos.todosimple.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
@@ -78,7 +79,10 @@ public class User {
 @OneToMany(mappedBy = "user")
 private List<Task> tasks=new ArrayList<Task>();
 
-
+    @JsonIgnore
+public List<Task> getTasks(){
+    return  this.tasks;
+}
 
 
 }
